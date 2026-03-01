195ceab2abb06a1e692d31a97fa1e02558cd6671
sunny-willow <sunny_willow@mail.ru>
2026-03-01 22:23:49 +0500

    Две идеи превратились в три

--- children.js
+++ children.js
@@ -2,6 +2,8 @@
 
 let mom = document.getElementById('mom');
 let child = document.getElementById('child');
+let children = document.getElementById('children');
+//локально работает, между прочим, и без строчек выше
 
 mom.addEventListener('click', function() {
     //window.location = 'mom/mom.html';
@@ -11,4 +13,9 @@ mom.addEventListener('click', function() {
 child.addEventListener('click', function() {
     //window.location = 'child/child.html';
     window.open('child/child.html', '_blank');
+})
+
+children.addEventListener('click', function() {
+    //window.location = 'children/children.html';
+    window.open('children/children.html', '_blank');
 })
